2de4ec0a7eb393a5fcf35212c329b489bc663ac9
Stephane Fellah <fellahst@gmail.com>
2025-07-28 01:23:40 -0400

    Initial commit

---
+++ respec-config.js
@@ -0,0 +1,41 @@
+var respecConfig = {
+  useW3CStyle: false,
+  specStatus: "unofficial",
+  shortName: "shacl-policy-language",
+  edDraftURI: "https://geoknoesis.github.io/shacl-policy-lang/index.html",
+  title: "SHACL Policy Language",
+  subtitle: "A Shapes-Based Access Control Policy Language",
+  publishDate: "2025-07-21",
+  editors: [
+    {
+      name: "Stephane Fellah",
+      company: "Geoknoesis LLC",
+      companyURL: "https://www.geoknoesis.com",
+    }
+  ],
+  github: "https://github.com/geoknoesis/shacl-policy-lang",
+  feedback: "stephanef@geoknoesis.com",
+  localBiblio: {
+    "OAC": {
+      title: "ODRL Profile for Access Control (OAC)",
+      href: "https://besteves4.github.io/odrl-access-control-profile/oac.html",
+      authors: ["B. Esteves"],
+      status: "Unofficial Draft",
+      date: "2023"
+    },
+    "ODRL-VC": {
+      title: "ODRL Profile using Verifiable Credential (ODRL-VC)",
+      href: "https://lists.w3.org/Archives/Public/public-odrl/2024Feb/0025.html",
+      authors: ["J. Cornejo"],
+      status: "Mailing list post",
+      date: "2024"
+    },
+    "SHACL-ACL": {
+      title: "SHACL-ACL: A SHACL-based Access Control Framework for RDF Data",
+      href: "https://2023.eswc-conferences.org/wp-content/uploads/2023/05/paper_Rohde_2023_SHACL-ACL.pdf",
+      authors: ["P. Rohde", "M. Martin", "T. Käfer"],
+      status: "In The Semantic Web: ESWC 2023 Satellite Events",
+      date: "2023"
+    }
+  }
+};
